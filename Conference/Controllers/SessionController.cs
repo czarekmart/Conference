2641720e2167ec9b3717853c8191f5053eb6f74b
czarekmart <czarekmart@gmail.com>
2014-06-06 21:17:01 -0700

    Added comment

--- Conference/Controllers/SessionController.cs
+++ Conference/Controllers/SessionController.cs
@@ -19,6 +19,7 @@ namespace Conference.Controllers
             return View(sessions);
         }
 
+		//********************************************
 		// GET: Session/Create
 		[HttpGet()]
 		public ActionResult Create()
