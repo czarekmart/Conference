8efcc27a5a100245a583b075bdd130a802e379bd
czarekmart <czarekmart@gmail.com>
2014-06-06 20:56:52 -0700

    Created MVC Conference

---
+++ Conference/Models/ConferenceContextInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Conference.Models
+{
+	public class ConferenceContextInitializer : DropCreateDatabaseAlways<ConferenceContext>
+	{
+		protected override void Seed(ConferenceContext context)
+		{
+			context.Sessions.Add(
+				new Session() { Title = "I Want Curry",
+								Abstract = "The life of curry lover",
+								Speaker = context.Speakers.Add(
+									new Speaker() { Name = "Cezar Mart",
+													EmailAddress = "czarekmart@gmail.com" })
+				});
+			context.SaveChanges();
+		}
+	}
+}
---
+++ Conference/Models/Speaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Conference.Models
+{
+	public class Speaker
+	{
+		public int SpeakerID { get; set; }
+
+		[Required(ErrorMessage="{0} is required")]
+		[Display(Name="Speaker Name")]
+		public string Name { get; set; }
+
+		[DataType(DataType.EmailAddress)]
+		public string EmailAddress { get; set; }
+
+		public virtual List<Session> Sessions	{ get; set; }
+	}
+}
